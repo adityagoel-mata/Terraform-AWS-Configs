42456591e507000db6acd794266f6ca9a051b04f
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-01-21 23:24:30 -0500

    commented the key_name temporarily

--- modules/EC2-instance/main.tf
+++ modules/EC2-instance/main.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "ec2-instance" {
   #host_id = var.host_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
-  key_name = var.key_name
+  #key_name = var.key_name
   tags = {
     Name = var.instance_name
   }
