85de1a420cffb5fa964e0e7e10cdf36a200aa525
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-01-21 23:23:20 -0500

    commented the host_id temporarily

--- modules/EC2-instance/main.tf
+++ modules/EC2-instance/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "ec2-instance" {
 
   ami = var.ami
-  host_id = var.host_id
+  #host_id = var.host_id
   instance_type = var.instance_type
   vpc_security_group_ids = var.vpc_security_group_ids
   key_name = var.key_name
