e809872a1f64ac502d400fdd35980b7ed8a153b8
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-01-30 12:26:25 -0500

    removed the vpc_id variable declaration

--- Project4/variables.tf
+++ Project4/variables.tf
@@ -3,11 +3,9 @@ variable "vpc_cidr_block" {}
 variable "vpc_name" {}
 
 #Public Subnet Variable declaration
-variable "public_subnet_vpc_id" {}
 variable "public_subnet_availability_zone" {}
 variable "public_subnet_Name" {}
 
 #Public Subnet Variable declaration
-variable "private_subnet_vpc_id" {}
 variable "private_subnet_availability_zone" {}
 variable "private_subnet_Name" {}
