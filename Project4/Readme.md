12050abfd4bf27eea7b452909edc32e3cfab7037
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-01-30 13:56:35 -0500

    Added Route Table association

--- Project4/Readme.md
+++ Project4/Readme.md
@@ -1,8 +1,8 @@
 This project should do the following steps:
-1. Create a VPC with IPv4 CIDR 10.0.0.0/16.
-2. The VPC should have 2 AZs.
-3. Both AZs should have 1 public subnet and 1 private subnet.
-3a. To create Public and Private subnets, we create 2 Route table and 1 Internet Gateway.
-3b. We assign IG to a route table. This route table is attached to the Public Subnet.
-3c. We attach the empty (local) route table to the Private Subnet.
-4. Create one NAT Gateway
+1. Create 1 VPC with IPv4 CIDR 10.0.0.0/16.
+2. Create 1 public subnet and 1 private subnet in different AZ within the VPC.
+3. Create 1 public route table and 1 private route table.
+4. Create 1 Internet Gateway. Attach it to the public route table.
+5. Attach the respective route tables to respective subnets.
+6. Create 1 NAT Gateway inside the public subnet.
+7. Add a new route in the private route table for NAT gateway.
--- Project4/main.tf
+++ Project4/main.tf
@@ -9,32 +9,32 @@ module "internet_gateway" {
   source = "../modules/Internet-Gateway"
 
   vpc_id = module.vpc.vpc_id
-  Name = var.internet_gateway_name
+  Name   = var.internet_gateway_name
   
 }
 
 module "public_route_table" {
-  source = "../modules/Route-Table"
+  source     = "../modules/Route-Table"
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id     = module.vpc.vpc_id    #This will by default attach a local GW as per VPC's CIDR
   cidr_block = var.public_route_table_cidr_block
   gateway_id = module.internet_gateway.internet_gateway_id
-  Name = var.public_route_table_Name
+  Name       = var.public_route_table_Name
 }
 
 
 module "private_route_table" {
-  source = "../modules/Route-Table"
+  source     = "../modules/Route-Table"
 
-  vpc_id = module.vpc.vpc_id
+  vpc_id     = module.vpc.vpc_id    #This will by default attach a local GW as per VPC's CIDR
   cidr_block = var.private_route_table_cidr_block
   gateway_id = var.private_route_table_gateway_id
-  Name = var.private_route_table_Name
+  Name       = var.private_route_table_Name
 }
 
 
 module "public_subnet" {
-  source = "../modules/Subnet"
+  source            = "../modules/Subnet"
 
   vpc_id            = module.vpc.vpc_id
   availability_zone = var.public_subnet_availability_zone
@@ -45,11 +45,27 @@ module "public_subnet" {
 
 
 module "private_subnet" {
-  source = "../modules/Subnet"
+  source            = "../modules/Subnet"
 
   vpc_id            = module.vpc.vpc_id
   availability_zone = var.private_subnet_availability_zone
   cidr_block        = var.private_subnet_cidr_block
   Name              = var.private_subnet_Name
   
+}
+
+
+module "public_route_table_association" {
+  source         = "../modules/Route-Table-Association"
+
+  subnet_id      = module.public_subnet.subnet_id
+  route_table_id = module.public_route_table.route_table_id
+}
+
+
+module "private_route_table_association" {
+  source         = "../modules/Route-Table-Association"
+
+  subnet_id      = module.private_subnet.subnet_id
+  route_table_id = module.private_route_table.route_table_id
 }
