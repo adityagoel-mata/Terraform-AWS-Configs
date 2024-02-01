4eaf325fe5909e727a0f4974d23b4ab38daaa108
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-02-01 12:58:37 -0500

    splat operator --try2

--- Project4/main.tf
+++ Project4/main.tf
@@ -40,7 +40,7 @@ module "public_route_table" {
 module "public_route_table_association" {
   source         = "../modules/Route-Table-Association"
 
-  subnet_id      = module.public_subnet.subnet_id
+  subnet_id      = module.public_subnet.subnet_id[*]
   route_table_id = module.public_route_table.route_table_id
 }
 
