b101006f9ba335bfd66fc0f943d3208034905f88
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-02-01 16:53:05 -0500

    modified the value of db_subnet_group_name

--- Project5/main.tf
+++ Project5/main.tf
@@ -126,7 +126,7 @@ module "mysql_db_instance" {
   password             = var.password
   parameter_group_name = var.parameter_group_name
   skip_final_snapshot  = var.skip_final_snapshot
-  db_subnet_group_name = module.public_subnet[*].Name
+  db_subnet_group_name = module.public_subnet.subnet_id[0]
   vpc_security_group_ids = [module.db_security_group.security_group_id]
 }
 
