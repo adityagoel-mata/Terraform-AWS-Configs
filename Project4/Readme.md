7285175eed69f3c08f00a96ee5a302e81ba2cc1d
adityagoel-mata <50823187+adityagoel-mata@users.noreply.github.com>
2024-02-01 11:05:31 -0500

    Reordered component creation

--- Project4/Readme.md
+++ Project4/Readme.md
@@ -1,8 +1,8 @@
 This project should do the following steps:
 1. Create 1 VPC with IPv4 CIDR 10.0.0.0/16.
-2. Create 1 public subnet and 1 private subnet in different AZ within the VPC.
+2. Create 2 public subnets and 2 private subnets in 2 different AZs within the VPC.
 3. Create 1 public route table and 1 private route table.
 4. Create 1 Internet Gateway. Attach it to the public route table.
 5. Attach the respective route tables to respective subnets.
-6. Create 1 NAT Gateway inside the public subnet.
-7. Add a new route in the private route table for NAT gateway.
+<!-- 6. Create 1 NAT Gateway inside the public subnet.
+7. Add a new route in the private route table for NAT gateway. -->
--- Project4/main.tf
+++ Project4/main.tf
@@ -5,6 +5,29 @@ module "vpc" {
   name = var.vpc_name
 }
 
+
+module "public_subnet" {
+  source            = "../modules/Subnet"
+
+  vpc_id            = module.vpc.vpc_id
+  availability_zone = var.public_subnet_availability_zone
+  cidr_block        = var.public_subnet_cidr_block
+  Name              = var.public_subnet_Name
+  
+}
+
+
+module "private_subnet" {
+  source            = "../modules/Subnet"
+
+  vpc_id            = module.vpc.vpc_id
+  availability_zone = var.private_subnet_availability_zone
+  cidr_block        = var.private_subnet_cidr_block
+  Name              = var.private_subnet_Name
+  
+}
+
+
 module "internet_gateway" {
   source = "../modules/Internet-Gateway"
 
@@ -33,28 +56,6 @@ module "private_route_table" {
 }
 
 
-module "public_subnet" {
-  source            = "../modules/Subnet"
-
-  vpc_id            = module.vpc.vpc_id
-  availability_zone = var.public_subnet_availability_zone
-  cidr_block        = var.public_subnet_cidr_block
-  Name              = var.public_subnet_Name
-  
-}
-
-
-module "private_subnet" {
-  source            = "../modules/Subnet"
-
-  vpc_id            = module.vpc.vpc_id
-  availability_zone = var.private_subnet_availability_zone
-  cidr_block        = var.private_subnet_cidr_block
-  Name              = var.private_subnet_Name
-  
-}
-
-
 module "public_route_table_association" {
   source         = "../modules/Route-Table-Association"
 
@@ -69,3 +70,4 @@ module "private_route_table_association" {
   subnet_id      = module.private_subnet.subnet_id
   route_table_id = module.private_route_table.route_table_id
 }
+
